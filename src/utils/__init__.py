b4f2402d4d1eff4f720851fb086556de9b347512
GbrlOl <gol197884266@gmail.com>
2025-11-02 03:31:51 -0300

    adding a path to our proposal for an improved RAG system in terms of implementation

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,9 +1,12 @@
 from .retrieval_fake import CustomFakeRetrieval
 from .retrieval_advanced_hybrid import EmbeddingBM25RerankRetriever
 from .excel_generator_retrieval import process_directory_json_files
+from .multi_embedd_utils import EmbeddingConfig, AsyncMultiEmbeddingRAGV2
 
 __all__ = [
     "CustomFakeRetrieval",
     "process_directory_json_files",
     "EmbeddingBM25RerankRetriever",
+    "EmbeddingConfig",
+    "AsyncMultiEmbeddingRAGV2",
 ]
