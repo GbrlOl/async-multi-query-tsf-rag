8aac83b6ebe8ed7050baeb1c21b9b04fc71fa25b
GbrlOl <gol197884266@gmail.com>
2026-05-12 17:11:36 -0400

    Agregué un nuevo módulo

--- src/evaluation_rag/__init__.py
+++ src/evaluation_rag/__init__.py
@@ -1,3 +1,4 @@
 from .automated_evaluation import auto_evaluation_rag
+from .automated_evaluation_V2 import auto_evaluation_rag_v2
 
-__all__ = ["auto_evaluation_rag"]
+__all__ = ["auto_evaluation_rag", "auto_evaluation_rag_v2"]
